9644a25db49c46de705d0c01e44f81a7b487b986
AndreiSimion97 <32966909+AndreiSimion97@users.noreply.github.com>
2017-11-28 17:59:40 +0200

    Add files via upload

--- seriac/poo/server/Server.java
+++ seriac/poo/server/Server.java
@@ -4,40 +4,58 @@
  * and open the template in the editor.
  */
 package seriac.poo.server;
+
+import chat.Message;
+import chat.PrivateMessage;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import seriac.poo.server.config.ServerConfig;
 import seriac.poo.server.exceptions.InvalidFormatException;
 import seriac.poo.server.exceptions.MissingKeyException;
 import seriac.poo.server.exceptions.UnknownKeyException;
-
 
 /**
  *
  * @author asimion
  */
 public class Server {
-   /**
-    * Method main - reprezinta de unde se porneste server-ul TCP 
-    * @param args
-    * @throws IOException - arunca exceptia daca a esuat conexiunea
-    * @throws UnknownKeyException - arunca exceptie daca in fisierul de configurare exista o cheie necunoscta
-    * @throws MissingKeyException - arunca exceptie daca lipsete o cheie in fisierul de configurare
-    * @throws InvalidFormatException - arunca excetie daca formatul datelor din fisier e invalid
-    * @throws ClassNotFoundException  - arunca exceptie daca clasa nu exista
-    */
-    public static void main (String args[]) throws IOException, UnknownKeyException, MissingKeyException, InvalidFormatException, ClassNotFoundException{
-     try {
+
+    ArrayList<ServerPeer> clientList = new ArrayList();
+    private ServerSocket mServerSocket;
+    private final int MAX_CLIENTS;
+
+    public Server(int tcpPort, int maxClients) throws IOException {
+        try {
+            this.mServerSocket = new ServerSocket(tcpPort);
+        } catch (IOException e) {
+
+        }
+        this.MAX_CLIENTS = maxClients;
+    }
+
+    /**
+     * Method main - reprezinta de unde se porneste server-ul TCP
+     *
+     * @param args
+     * @throws IOException - arunca exceptia daca a esuat conexiunea
+     * @throws UnknownKeyException - arunca exceptie daca in fisierul de
+     * configurare exista o cheie necunoscta
+     * @throws MissingKeyException - arunca exceptie daca lipsete o cheie in
+     * fisierul de configurare
+     * @throws InvalidFormatException - arunca excetie daca formatul datelor din
+     * fisier e invalid
+     * @throws ClassNotFoundException - arunca exceptie daca clasa nu exista
+     */
+    public static void main(String args[]) throws IOException, UnknownKeyException, MissingKeyException, InvalidFormatException, ClassNotFoundException {
+
+        try {
             ServerConfig objectConfig = new ServerConfig();
-            ServerSocket tcpSocket = new ServerSocket(objectConfig.getTcpPort());
-            Socket socket =  tcpSocket.accept();
-            if(socket.isConnected()){
-                ServerPeer srvPeer = new ServerPeer(socket);
-                srvPeer.run();
-            }
-            
-        } catch (IOException e){
+            Server serverMain = new Server(objectConfig.getTcpPort(), objectConfig.getMaxClients());
+            serverMain.listen(serverMain);
+        } catch (IOException e) {
             System.out.println("Conexiunea s-a oprit!");
         } catch (InvalidFormatException e) {
             System.out.println(e.getMessage());
@@ -47,6 +65,43 @@ public class Server {
             System.out.println(e.getMessage());
         }
 
+    }
+
+    public void listen(Server server) throws IOException {
+        while (true) {
+            if (clientList.size() < this.MAX_CLIENTS) {
+                ServerPeer client = new ServerPeer(server, mServerSocket.accept());
+                clientList.add(client);
+                (new Thread(client)).start();
+            }
+        }
+    }
+
+    public synchronized void dispatch(Message mess) {
+        for (ServerPeer client : clientList) {
+            if (mess instanceof PrivateMessage) {
+                if (client.getUsername().equals(((PrivateMessage) mess).getRecipient())) {
+                    try {
+                        ObjectOutputStream objectStream = client.getOOS();
+                        objectStream.writeObject(mess);
+                    } catch (Exception e) {
+
+                    }
+                }
+            } else {
+                try {
+                    ObjectOutputStream objectStream = client.getOOS();
+                    objectStream.writeObject(mess);
+                } catch (Exception e) {
+
+                }
+            }
+        }
+    }
+
+    
+    public synchronized void removeClient(ServerPeer serverPeer) {
+       clientList.remove(serverPeer);
     }
     
 }
--- seriac/poo/server/ServerPeer.java
+++ seriac/poo/server/ServerPeer.java
@@ -10,36 +10,84 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 import chat.*;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.ObjectOutputStream;
+
 /**
  *
  * @author asimion
  */
-public class ServerPeer {
-    private Socket mConnected ;
-    private DataInputStream mDIS;
-    private ObjectInputStream mOIS;
+public class ServerPeer implements Runnable {
+
+    private final Socket mSocketConnected;
+    private  Server mServer;
+    private  String mUsername;
+    //private  DataInputStream mDIS;
+    //private ObjectInputStream mOIS;
+    private  DataOutputStream mDOS;
+    private ObjectOutputStream mOOS;
+   
     /**
      * Constructor class ServerPeer
-     * 
-     * @param connected reprezinta Socket-ul pe care e connectat clientul 
+     *
+     * @param connected reprezinta Socket-ul pe care e connectat clientul
      */
-    public ServerPeer(Socket connected) throws IOException{
-        mConnected = connected;
-        mDIS = new DataInputStream(mConnected.getInputStream());
-        mOIS = new ObjectInputStream(mDIS);
+    public ServerPeer(Server server, Socket socket) throws IOException {
+        mSocketConnected = socket;
+        mServer = server;
+        mDOS = new DataOutputStream(mSocketConnected.getOutputStream());
+        mOOS = new ObjectOutputStream(mDOS);
     }
+
     /**
      * Method run() - deserializeaza obiecte de tip Message
+     *
      * @throws IOException
-     * @throws ClassNotFoundException 
+     * @throws ClassNotFoundException
      */
-    public void run() throws IOException, ClassNotFoundException{
-        while(mConnected.isConnected()){
-           // DataInputStream _in = new DataInputStream(mConnected.getInputStream());
-            //ObjectInputStream _objIn = new ObjectInputStream(_in);
-            Message _mObj = (Message)mOIS.readObject();
-            System.out.println(_mObj.toString());   
+
+    @Override
+    public void run() {
+       try{
+            Message message;//creez o referinta la un obiect de tip Messae
+            DataInputStream _dis = new DataInputStream(mSocketConnected.getInputStream());
+            ObjectInputStream _oos = new ObjectInputStream(_dis);
+            while(true){
+                message = (Message) _oos.readObject();//citesc de pe strimul de iersire a serverului un mesaj
+                mUsername = message.getSender();//extrag expeditorul
+                System.out.println(message.toString());//afisez mesajul
+                if(message instanceof PrivateMessage){//verific daca este mesaj privat sau nu
+                    sendMessage(message);//serializez mesajul
+                    mServer.dispatch(message);//trimit unde trebuie mesajul
+                }
+                else{
+                    mServer.dispatch(message);//trimit mai departe mesajul
+                }
+            }
+        } catch (EOFException e){
+            mServer.removeClient(this);//sterg clientul daca nu mai e loc pe server
+        } catch (IOException e){
+            mServer.removeClient(this);//sterg clientul daca nu s-a putut conectat
+            System.err.println("Conexiunea clientului resetata: " + e.getMessage());
+        } catch (ClassNotFoundException e){
+            System.err.println("Obiect necunoscut primit!");
         }
     }
+
+    public void sendMessage(Message message) throws IOException {
+        mOOS.writeObject(message);
+    }
+
+    public Socket getSocket(){
+        return this.mSocketConnected;
+    }   
+ 
+    public ObjectOutputStream getOOS(){
+        return this.mOOS;
+    }
     
+    public String getUsername() {
+        return this.mUsername;
+    }
 }
