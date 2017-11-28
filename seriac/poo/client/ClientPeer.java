56b43296685c937246b64281346113d96e6d36c9
AndreiSimion97 <32966909+AndreiSimion97@users.noreply.github.com>
2017-11-28 17:57:05 +0200

    Add files via upload

--- seriac/poo/client/ClientPeer.java
+++ seriac/poo/client/ClientPeer.java
@@ -7,56 +7,88 @@ package seriac.poo.client;
 
 import java.net.Socket;
 import chat.*;//importez de unde am creat clasele Message si PrivateMessage
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author asimion
  */
-public class ClientPeer {
-       /**
-        * Campuri privte ce definesc clasa ClientPeer
-        */
-    private Socket mConnectedClient;
-    private String mSender;   
-    private DataOutputStream mDOS ;
-    private ObjectOutputStream mOOS;
+public class ClientPeer implements Runnable {
+
     /**
-         * Constructor class ClientPeer
-         * @param sender String care reprezinta expeditorul
-         * @param connectedClient Socket pe care este coneatat un client
-        */
-        public ClientPeer(String sender,Socket connectedClient) throws IOException{
-               mSender = sender;
-               mConnectedClient = connectedClient;
-               mDOS = new DataOutputStream(mConnectedClient.getOutputStream());
-               mOOS = new ObjectOutputStream(mDOS);
+     * Campuri privte ce definesc clasa ClientPeer
+     */
+    private Socket mSocketClient;
+    private String mSender;
+    private static DataOutputStream mDOS;
+   // private static DataInputStream mDIS;
+    private  ObjectOutputStream mOOS;
+    //private  ObjectInputStream mOIS;
+
+    /**
+     * Constructor class ClientPeer
+     *
+     * @param sender String care reprezinta expeditorul
+     * @param connectedClient Socket pe care este coneatat un client
+     */
+    public ClientPeer(String sender, Socket socket) throws IOException {
+        mSender = sender;
+        mSocketClient = socket;
+        mDOS = new DataOutputStream(mSocketClient.getOutputStream());
+        mOOS = new ObjectOutputStream(mDOS);
+        //mDIS = new DataInputStream(mConnectedClient.getInputStream());
+        //mOIS = new ObjectInputStream(mDIS);
+    }
+
+    public void run() {
+        try{    
+            DataInputStream dis = new DataInputStream(mSocketClient.getInputStream());
+            ObjectInputStream ois = new ObjectInputStream(dis);
+            while(true){
+                System.out.println(ois.readObject().toString().trim());
+            }
+        }catch(IOException | ClassNotFoundException e){
+            
         }
-        /**
-         * Method sendMessage
-         * Serializeaza pe Socket obiecte de tip Message
-         * @param message String ce reprezinta mesajul trimis
-         * @throws  IOException atunci cand nu poate/are permisiunea de a scrie pe socket
-         */
-        public void sendMessage(String message) throws IOException{
-            Message _message = new Message(mSender, message);
-            //DataOutputStream outToServer = new DataOutputStream(mConnectedClient.getOutputStream());
-            //ObjectOutputStream _objServerStream = new ObjectOutputStream(outToServer);
-           mOOS.writeObject(_message);
-        }
-        /**
-         * Method sendMessage 
-         * Serializeaza pe Socket obiecte de tip PrivateMessage
-         * @param message String ce reprezinta mesajul trimis
-         * @param recipient String ce reprezinta destinatarul
-         * @throws IOException atunci cand nu poate/are permisiunea de a scrie pe socket
-         */
-        public void sendMessage(String message, String recipient) throws IOException{
-            PrivateMessage _privateMessage = new PrivateMessage(recipient, mSender, message);
-            //DataOutputStream outToServer = new DataOutputStream(mConnectedClient.getOutputStream());
-            //ObjectOutputStream _objServerStream = new ObjectOutputStream(outToServer);
-            mOOS.writeObject(_privateMessage);
-        }
+    }
+
+    /**
+     * Method sendMessage Serializeaza pe Socket obiecte de tip Message
+     *
+     * @param message String ce reprezinta mesajul trimis
+     * @throws IOException atunci cand nu poate/are permisiunea de a scrie pe
+     * socket
+     */
+    public void sendMessage(String message) throws IOException {
+        Message _message = new Message(mSender, message);
         
+        mOOS.writeObject(_message);
+    }
+
+    /**
+     * Method sendMessage Serializeaza pe Socket obiecte de tip PrivateMessage
+     *
+     * @param message String ce reprezinta mesajul trimis
+     * @param recipient String ce reprezinta destinatarul
+     * @throws IOException atunci cand nu poate/are permisiunea de a scrie pe
+     * socket
+     */
+    public  void sendMessage(String message, String recipient) throws IOException {
+        PrivateMessage _privateMessage = new PrivateMessage(recipient, mSender, message);
+        
+        mOOS.writeObject(_privateMessage);
+    }
+
+   
+
+   
 }
--- seriac/poo/client/TextClient.java
+++ seriac/poo/client/TextClient.java
@@ -15,47 +15,64 @@ import java.net.Socket;
  * @author asimion
  */
 public class TextClient {
+
     /**
-     * Metoda statica main care porneste TextClient ce se conecteaza pe portul 9000 localhost '127.0.0.1'
-     * si serializeaza obiectele de tip Message si PrivateMessage prin apeluri ale obictului de tip 
-     * ClientPeer la metodele sendMessage
+     * Metoda statica main care porneste TextClient ce se conecteaza pe portul
+     * 9000 localhost '127.0.0.1' si serializeaza obiectele de tip Message si
+     * PrivateMessage prin apeluri ale obictului de tip ClientPeer la metodele
+     * sendMessage
+     *
      * @param args
-     * @throws IOException 
+     * @throws IOException
      */
-        public static void main(String args[]) throws IOException{
-           
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in) );
-            
+    public static void main(String args[]) throws IOException, InterruptedException, ClassNotFoundException {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
             System.out.print("Username: ");
             String username = in.readLine();
-            
+
             Socket clientSocket = new Socket("127.0.0.1", 9000);
-            
+
             ClientPeer client = new ClientPeer(username, clientSocket);
-           String line ; 
-           do{
-             line = in.readLine();     
-             if(line.contains("/w")){
-                 String []information = line.split(" ");
-                 client.sendMessage(Concatenate(information, 2) , information[1]);
-             }else{
-                 if(!line.equals("/q")) client.sendMessage(line);
-             }
-           }while(!line.equals("/q")); 
-           
-        
-           clientSocket.close();
+           ( new Thread(client)).start();
+            String line;
+            do {
+
+                line = in.readLine();
+
+                if (line.contains("/w") && !line.equals("/w")) {
+                    String[] information = line.split(" ");
+
+                    client.sendMessage(Concatenate(information, 2), information[1]);
+
+                } else {
+                    if (!line.equals("/q")) {
+
+                        client.sendMessage(line);
+                    } else {
+                        clientSocket.close();
+                        break;
+                    }
+                }
+
+            } while (!line.equals("/q"));
+        } catch (IOException e) {
+
         }
-        /**
-         * Method Concatenate - produce si intoarce un String reprezentat de mesajul prin concatenarea
-         * cuvintelor ce au fost scrise
-         * @param arr reprezinta un array de String 
-         * @param index reprezinta indexul array-ului de unde se incepe concatenarea
-         * @return message
-         */
-    private static String Concatenate(String []arr, int index){
+    }
+
+    /**
+     * Method Concatenate - produce si intoarce un String reprezentat de mesajul
+     * prin concatenarea cuvintelor ce au fost scrise
+     *
+     * @param arr reprezinta un array de String
+     * @param index reprezinta indexul array-ului de unde se incepe concatenarea
+     * @return message
+     */
+    private static String Concatenate(String[] arr, int index) {
         String message = "";
-        for(int i = index; i < arr.length; i++){
+        for (int i = index; i < arr.length; i++) {
             message = message + (arr[i] + " ");
         }
         return message;
